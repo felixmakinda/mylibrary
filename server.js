7632d6ed9f321970f7134ba9d2a721b275589ae8
felix_makinda <felixmakinda@gmail.com>
2023-02-17 13:03:41 +0300

    env changed

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(expressLayouts);
 app.use(express.static("public"));
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
 });
 const db = mongoose.connection;
